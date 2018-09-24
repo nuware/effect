c8262fd2859c79ffd0369412609964c57dcd8924
Dmitry Dudin <dima.dudin@gmail.com>
2018-09-24 12:54:05 +0700

    Implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-const Effect = () => {
+import {
+  K
+} from '@nuware/functions'
 
-}
+const Effect = (f) => ({
+  map: (g) => Effect((x) => g(f(x))),
+  join: (x) => f(x),
+  chain: (g) => Effect(f).map(g).join(),
+  ap: (m) => m.map((g) => g(f())),
+  inspect: () => `Effect(${f})`
+})
+
+Effect.of = (value) => Effect(K(value))
 
 export default Effect
